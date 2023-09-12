33a8e17d50783b67930317d60db3e8abd349bcb9
causticrystals <b.papineau-couture@share.epsb.ca>
2023-09-12 12:37:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ SCREEN_SIZE = (700, 500)
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
 # Draw stickman function
-def draw_stickman(color) :
+def draw_stickman(color, x, y) :
     #head
-    pygame.draw.circle(screen, color, (300, 180), 50, 3)
+    pygame.draw.circle(screen, color, (x + 80, y + 80), 50, 3)
     #body
-    pygame.draw.line(screen, color, (300, 230), (300, 350), 3)
+    pygame.draw.line(screen, color, (x + 80, y + 130), (300, 350), 3)
     #arms
     pygame.draw.line(screen, color, (300, 240), (230, 300), 3)
     pygame.draw.line(screen, color, (300, 240), (360, 300), 3)
@@ -27,7 +27,7 @@ while run:
     screen.fill("white")
 
     #draw shapes
-    draw_stickman("black")
+    draw_stickman("black") # complete other coords
 
     for event in pygame.event.get(): 
         #when user clicks X on window so the window isnt stuck open
