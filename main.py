c7d10a0f30264446cbefbb295bb3ebe8046badda
causticrystals <b.papineau-couture@share.epsb.ca>
2023-09-12 13:16:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ import pygame
 pygame.init()
 
 # Canvas settings and variables
-SCREEN_SIZE = (700, 500)
+SCREEN_SIZE = (900, 700)
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
 # Draw stickman function
-def draw_stickman(color, x, y) :
+def draw_stickman(color, x, y) : #(230, 100)
     #head
-    pygame.draw.circle(screen, color, (x + 80, y + 80), 50, 3)
+    pygame.draw.circle(screen, color, (x + 70, y + 80), 50, 3)
     #body
-    pygame.draw.line(screen, color, (x + 80, y + 130), (300, 350), 3)
+    pygame.draw.line(screen, color, (x + 70, y + 130), (x + 70, y + 250), 3)
     #arms
-    pygame.draw.line(screen, color, (300, 240), (230, 300), 3)
-    pygame.draw.line(screen, color, (300, 240), (360, 300), 3)
+    pygame.draw.line(screen, color, (x + 70, y + 140), (x, y + 200), 3)
+    pygame.draw.line(screen, color, (x + 70, y + 140), (x + 130, y + 200), 3)
     #legs
-    pygame.draw.line(screen, color, (300, 350), (260, 430), 3)
-    pygame.draw.line(screen, color, (300, 350), (340, 430), 3)
+    pygame.draw.line(screen, color, (x + 70, y + 250), (x + 30, y + 330), 3)
+    pygame.draw.line(screen, color, (x + 70, y + 250), (x + 110, y + 330), 3)
+    #face
+    pygame.draw.arc(screen, color, (x + 45, y + 60, 50 ,50), 3.14, 0, width = 3)
+    pygame.draw.circle(screen, color, (x + 50, y + 70), 5)
+    pygame.draw.circle(screen, color, (x + 90, y + 70), 5)
+
 
 
 # Open canvas
@@ -27,7 +32,8 @@ while run:
     screen.fill("white")
 
     #draw shapes
-    draw_stickman("black") # complete other coords
+    draw_stickman("crimson", 230, 100)
+    draw_stickman("darkgoldenrod2", 370, 100)
 
     for event in pygame.event.get(): 
         #when user clicks X on window so the window isnt stuck open
